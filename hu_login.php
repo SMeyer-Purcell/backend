5a60278b7330f772495c66c4e06d8ec37b967014
SMeyer-Purcell <60480127+SMeyer-Purcell@users.noreply.github.com>
2020-04-01 08:08:16 -0400

    Update hu_login.php

--- hu_login.php
+++ hu_login.php
@@ -26,9 +26,12 @@ if (isset($_POST['submit'])) {
   $password = mysql_escape_string($_POST['hu_id']);
 
 // Checks to see if user entered in their email and id
+// Tells user to input their email and password 
 if (!$_POST['hu_email'] || !$_POST['hu_id']) {
   echo error('Fill in required fields');
 }
+      
+// Selects user's email and password from database 
 else {
   $sql ="SELECT * from users WHERE $uemail = 'hu_email' AND $password = 'hu_id';";
   $stmt = mysqli_stmt_init($conn);
@@ -41,14 +44,21 @@ else {
     $result = mysqli_stmt_get_result($stmt);
     if ($row = mysqli_fetch_assoc()) {
         $pwdCheck = password_verify($password, $row['hu_id']);
+          
+// Verifies that the user entered the correct password  
+// If user entered the wrong password, the error message will tell them to enter the correct one
+          
         if ($pwdCheck == false)
           echo error('wrong password');
         }
+        
+// If the user's password is verified, a new session is created
         else if ($pwdCheck == true)
           session_start();
           $_SESSION['uemail'] = $row[hu_email];
           $_SESSION['uid'] = $row[hu_id];
-
+        
+// Directs user to the main page of the website
           header("location: mainpage.html");
         }
     }
